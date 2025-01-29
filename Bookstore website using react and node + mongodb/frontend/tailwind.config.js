7d7d04035b14dd30c9ac42ced6dcd9e1da1c9d15
Fiachra2002 <147062481+Fiachra2002@users.noreply.github.com>
2025-01-29 14:58:32 +0000

    Add files via upload

---
+++ Bookstore website using react and node + mongodb/frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html",        
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  darkMode: 'class', 
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
